f1560da9448d0b56af439862df8c463e10835bd0
Kook Metawee <metawee.77@gmail.com>
2026-06-08 17:48:08 +0700

    Update LivePreview.tsx

--- components/LivePreview.tsx
+++ components/LivePreview.tsx
@@ -243,7 +243,7 @@ export default function LivePreview({ html }: { html: string }) {
 
             hr { color: #fff; background: #303030; height: 1px; border: 0px; margin: 1em 0; }
             a { color: rgb(43, 120, 255); text-decoration: none; transition: .3s; }
-            a:hover { text-decoration: underline; }
+            a:hover { text-decoration: none; }
             .hidden-content-title {
                 margin-top: 10px;
                 font-size: 22px;
